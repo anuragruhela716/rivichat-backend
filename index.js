141faee0368fa42fd43002d96a8f5c951023f665
anuragruhela716 <anuragruhela25@gmail.com>
2025-08-04 00:25:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+Const express = require("express");
+Const app = express();
+Const port = process.env.PORT || 3000;
 
 app.get("/api/test", (req, res) => {
   res.json({ status: "ok", message: "Backend connected successfully!" });
 });
 
 app.listen(port, () => {
-  console.log(Server running on port ${port});
+  console.log(`Server running on port ${port}`);
 });
